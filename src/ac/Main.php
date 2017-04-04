82972d284109aae327b69c0f88a049bb4ba61c5a
G2P <guyza01.2547@gmail.com>
2017-04-04 15:58:04 +0700

    Change from kick -> ban and translate

--- src/ac/Main.php
+++ src/ac/Main.php
@@ -11,6 +11,8 @@
     use pocketmine\entity\Effect;
     use pocketmine\utils\TextFormat;
     use pocketmine\network\protocol\UpdateAttributesPacket;
+    use \pocketmine\utils\TextFormat;
+
 
     class Main extends PluginBase implements Listener {
 
@@ -33,21 +35,21 @@
             $player = $event->getPlayer();
             $packet = $event->getPacket();
             if($packet instanceof UpdateAttributesPacket){ #กัน Player ส่ง Data AttributesPacket เพราะมันเกี่ยวกันเลือด อาหาร Speed การเดิม //พบคน Hack อาการนี้น้อย
-                var_dump("ไอสัส ".$player->getName()." Hack AttributesPacket");
-                $player->kick("ไอสัส HACK AttributesPacket");
+                var_dump($player->getName()." Hack AttributesPacket");
+                $player->ban("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
             }
             if ($packet instanceof AdventureSettingsPacket) { 
                 switch ($packet->flags) { 
                     case 614: #กัน Fly ชั้นที่ 1 เช้ค packet ใช้ปุ่มลอย ไม่เตะมั่ว 100%
                         if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
-                            var_dump("ไอสัส ".$player->getName()." Hack บิน");
-                            $player->kick("ไอสัส HACK บิน");
+                            var_dump($player->getName()." Hack flying");
+                            $player->ban("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
                         }
                         break;
                     case 102: #กัน Fly ชั้นที่ 1 เช้ค packet ใช้ปุ่มลอย ไม่เตะมั่ว 100%
                         if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
-                            var_dump("ไอสัส ".$player->getName()." Hack บิน");
-                            $player->kick(TextFormat::RED."ไอสัส HACK บิน");
+                            var_dump($player->getName()." Hack flying");
+                            $player->ban("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
                         }
                         break;
                     default:
@@ -61,8 +63,8 @@
             if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp()){
                 if(!$player->hasEffect(Effect::SPEED)){ #กัน TapTelePort เตะทันทีที่วาป โอกาศ 80% กันได้ กัน Speed ได้แค่ 10% ไม่เตะมั่ว 100%
                     if(abs(round(($event->getTo()->getX() - $event->getFrom()->getX()) * ($event->getTo()->getZ() - $event->getFrom()->getZ()),3)) >= 1){
-                        var_dump("ไอสัส ".$player->getName()." Hack วิ่ง");
-                        $player->kick(TextFormat::RED."ไอสัส Hack วิ่ง");
+                        var_dump($player->getName()." Hack speed");
+                        $player->ban("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
                     }
                 }
                 if(!$player->getAllowFlight() and !$player->hasEffect(Effect::JUMP)){ #กัน โดดสูงๆ โอกาศป้องกันขั้นนี้ 90% ไม่เตะมั่ว 100%
@@ -72,8 +74,8 @@
                         $this->players[$player->getName()] = 0;
                     }
                     if($this->players[$player->getName()] >= 3){
-                        var_dump("ไอสัส ".$player->getName()." Hack โดดสูง หรือ บืน");
-                        $player->kick(TextFormat::RED."ไอสัส Hack โดดสูง หรือ บิน");
+                        var_dump($player->getName()." Hack Jump or Flying");
+                        $player->ban("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
                     }
                 }
                 
