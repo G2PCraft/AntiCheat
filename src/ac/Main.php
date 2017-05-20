a33383458193589da5a926d010f33043121d6800
G2P <guyza01.2547@gmail.com>
2017-05-19 23:41:40 -0700

    Update Main.php

--- src/ac/Main.php
+++ src/ac/Main.php
@@ -1,83 +1,139 @@
 <?php
-
-    namespace ac;
-
-    use pocketmine\event\Listener;
-    use pocketmine\plugin\PluginBase;
-    use pocketmine\network\protocol\AdventureSettingsPacket;
-    use pocketmine\event\server\DataPacketReceiveEvent;
-    use pocketmine\event\player\PlayerJoinEvent;
-    use pocketmine\event\player\PlayerMoveEvent;
-    use pocketmine\entity\Effect;
-    use pocketmine\utils\TextFormat;
-    use pocketmine\network\protocol\UpdateAttributesPacket;
-
-
+namespace ac;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\network\protocol\AddPlayerPacket;
+use pocketmine\network\protocol\RemoveEntityPacket;
+use pocketmine\network\protocol\AdventureSettingsPacket;
+use pocketmine\network\protocol\UpdateAttributesPacket;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\UUID;
+use pocketmine\item\Item;
+use pocketmine\entity\Entity;
+use pocketmine\Player;
+use pocketmine\math\Vector3;
+use pocketmine\block\Block;
     class Main extends PluginBase implements Listener {
+        public $movePlayers = [];
+        public $point = [];
+        public $npcs = [];
 
-        #เอาไปใช้ดีก็ขอบคุณกันนิดนึง 
-        #By TIGER OWNER APPLECRAFt
-
-        #AppleCraft ip : applecraft.cf port : 19132
-
-        public $players = [];
 
         public function onEnable() {
+            $id = Entity::$entityCount++;
+            $uuid = UUID::fromRandom();
+            $pkadd = new AddPlayerPacket();
+            $pkadd->uuid = $uuid;
+            $pkadd->username = "";
+            $pkadd->eid = $id;
+            $pkadd->x = 0;
+            $pkadd->y = 0;
+            $pkadd->z = 0;
+            $pkadd->yaw = 0;
+            $pkadd->pitch = 0;
+            $pkadd->item = Item::fromString(0);;
+            $flags = 0;
+            $flags |= 1 << 5;
+            $flags |= 1 << 14;
+            $flags |= 1 << 15;
+            $flags |= 1 << 16;
+            $pkadd->metadata = [ 
+                Entity::DATA_FLAGS => [Entity::DATA_TYPE_LONG, $flags],
+                Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, ""]
+            ];
+            $pkremove = new RemoveEntityPacket();
+            $pkremove->eid = $id;
+            $this->npcs["add"] = $pkadd;
+            $this->npcs["id"] = $id;
+            $this->npcs["remove"] = $pkremove;
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new CheckTask($this), 20);
         }
 
-        public function onPlayerJoin(PlayerJoinEvent $event){
-            $this->players[$event->getPlayer()->getName()] = 0;
+
+        public function onPlayerKick(PlayerKickEvent $event){
+            if($event->getReason() === "Sorry, hack mods are not permitted on Steadfast... at all."){
+                //$event->setCancelled(true);
+            }
     	}
+
+
+        public function onPlayerJoin(PlayerJoinEvent $event){
+            $player = $event->getPlayer();
+            $this->movePlayers[$player->getName()]["distance"] = (float) 0;
+            $this->point[$player->getName()]["distance"] = (float) 0;
+            $this->movePlayers[$player->getName()]["fly"] = (float) 0;
+            $this->point[$player->getName()]["fly"] = (float) 0;
+    	}
+
+
+        public function onPlayerQuit(PlayerQuitEvent $event){
+            $player = $event->getPlayer();
+            unset($this->movePlayers[$player->getName()]);
+            unset($this->point[$player->getName()]);
+    	}
+
+
+        public function onPlayerMove(PlayerMoveEvent $event){
+            $player = $event->getPlayer();
+            $oldPos= $event->getFrom();
+		    $newPos = $event->getTo();
+            if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
+                $this->movePlayers[$player->getName()]["distance"] += sqrt(($newPos->getX() - $oldPos->getX()) ** 2 + ($newPos->getZ() - $oldPos->getZ()) ** 2);
+            }
+    	}
+
 
         public function onRecieve(DataPacketReceiveEvent $event) {
             $player = $event->getPlayer();
             $packet = $event->getPacket();
-            if($packet instanceof UpdateAttributesPacket){ #กัน Player ส่ง Data AttributesPacket เพราะมันเกี่ยวกันเลือด อาหาร Speed การเดิม //พบคน Hack อาการนี้น้อย
-                var_dump($player->getName()." Hack AttributesPacket");
-                $player->kick("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
+            $player = $event->getPlayer();
+            if($packet instanceof UpdateAttributesPacket){ 
+                $this->getServer()->broadcastMessage(TextFormat::GOLD . $player->getName() . " was ban becuase using Hack!");
+                $this->getServer()->getNameBans()->addBan($player->getName(), "No hacking!");
             }
-            if ($packet instanceof AdventureSettingsPacket) { 
-                switch ($packet->flags) { 
-                    case 614: #กัน Fly ชั้นที่ 1 เช้ค packet ใช้ปุ่มลอย ไม่เตะมั่ว 100%
-                        if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
-                            var_dump($player->getName()." Hack flying");
-                            $player->kick("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
-                        }
-                        break;
-                    case 102: #กัน Fly ชั้นที่ 1 เช้ค packet ใช้ปุ่มลอย ไม่เตะมั่ว 100%
-                        if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
-                            var_dump($player->getName()." Hack flying");
-                            $player->kick("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
-                        }
-                        break;
-                    default:
-                        break;
+            if($packet instanceof SetPlayerGameTypePacket){ 
+                $player->kick(TextFormat::RED."#HACK SetPlayerGameTypePacket");
+                $this->getServer()->broadcastMessage(TextFormat::GOLD . $player->getName() . " was ban becuase using Hack!");
+                $this->getServer()->getNameBans()->addBan($player->getName(), "No hacking!");
+            }
+            if($packet instanceof AdventureSettingsPacket){
+                if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
+                    switch ($packet->flags){ //Packet ส่งขอลอย
+                        case 614:
+                        case 615:
+                        case 103:
+                        case 102:
+                        case 38:
+                        case 39:
+                            $this->getServer()->broadcastMessage(TextFormat::GOLD . $player->getName() . " was ban becuase using Hack!");
+                            $this->getServer()->getNameBans()->addBan($player->getName(), "No hacking!");
+                            break;
+                        default:
+                            break;
+                    }
+                    if((($packet->flags >> 9) & 0x01 === 1) or (($packet->flags >> 7) & 0x01 === 1) or (($packet->flags >> 6) & 0x01 === 1)){
+                        $this->getServer()->broadcastMessage(TextFormat::GOLD . $player->getName() . " was ban becuase using Hack!");
+                        $this->getServer()->getNameBans()->addBan($player->getName(), "No hacking!");
+                    }
                 }
+                if (($packet->allowFlight || $packet->isFlying) && $player->getAllowFlight() && $player->isSpectator() && $player->isOp() !== true) {
+                        $this->getServer()->broadcastMessage(TextFormat::GOLD . $player->getName() . " was ban becuase using Hack!");
+                    $this->getServer()->getNameBans()->addBan($player->getName(), "No hacking!");
+                }
+                if ($packet->noClip && $player->isSpectator() !== true) {
+                    $this->getServer()->broadcastMessage(TextFormat::GOLD . $player->getName() . " was ban becuase using Hack!");
+                    $this->getServer()->getNameBans()->addBan($player->getName(), "No hacking!");
+                }
+            $player->sendSettings();
+            $event->setCancelled();
             }
         }
-
-        public function PlayerMove(PlayerMoveEvent $event){
-            $player = $event->getPlayer();
-            if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp()){
-                if(!$player->hasEffect(Effect::SPEED)){ #กัน TapTelePort เตะทันทีที่วาป โอกาศ 80% กันได้ กัน Speed ได้แค่ 10% ไม่เตะมั่ว 100%
-                    if(abs(round(($event->getTo()->getX() - $event->getFrom()->getX()) * ($event->getTo()->getZ() - $event->getFrom()->getZ()),3)) >= 1){
-                        var_dump($player->getName()." Hack speed");
-                        $player->kick("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
-                    }
-                }
-                if(!$player->getAllowFlight() and !$player->hasEffect(Effect::JUMP)){ #กัน โดดสูงๆ โอกาศป้องกันขั้นนี้ 90% ไม่เตะมั่ว 100%
-                    if(round($event->getTo()->getY() - $event->getFrom()->getY(),3) >= 0.375) {
-                        $this->players[$player->getName()] ++;
-                    }else{
-                        $this->players[$player->getName()] = 0;
-                    }
-                    if($this->players[$player->getName()] >= 3){
-                        var_dump($player->getName()." Hack Jump or Flying");
-                        $player->kick("/n" . TextFormat::RED . "Hacking is not allowed on G2PCraft");
-                    }
-                }
-                
-            }
-	    }
     }
